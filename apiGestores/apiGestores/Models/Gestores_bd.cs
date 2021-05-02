67c606835d6c45f1c60f598069e94a8b6dba6769
Alexander Flores <58058343+alex160799@users.noreply.github.com>
2021-05-02 17:50:36 -0600

    get y post terminado

---
+++ apiGestores/apiGestores/Models/Gestores_bd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiGestores.Models
+{
+    public class Gestores_bd
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string nombre { get; set; }
+
+        public int lanzamiento { get; set; }
+
+        public string desarrollador { get; set; }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {Modal,ModalBody,ModalFooter,ModalHeader} from 'reactstrap';
 
 function App() {
+
+  const baseUrl="https://localhost:44388/api/gestores";
+  const [data ,setData]=useState([]);  
+
+  const [modalInsertar , setModalInsertar]=useState(false);
+
+  const [gestorSeleccionado, setGestorSeleccionado]=useState({
+    id: '',
+    nombre: '',
+    lanzamiento: '',
+    desarrollador: ''
+  })
+
+  const handleChange=e=>{
+    const {name, value}=e.target;
+    setGestorSeleccionado({
+      ...gestorSeleccionado,
+      [name]: value
+    });
+    console.log(gestorSeleccionado);
+  }
+
+  const abrirCerrarModalInsertar=()=>{
+    setModalInsertar(!modalInsertar);
+  }
+
+  const peticionGet=async()=>{
+    await axios.get(baseUrl)
+    .then((response) => {
+
+      setData(response.data);
+
+    }).catch((err) => {
+
+      console.log(err)
+    });
+  }
+
+
+  const peticionPost=async()=>{
+    delete gestorSeleccionado.id;
+    gestorSeleccionado.lanzamiento=parseInt(gestorSeleccionado.lanzamiento);
+    await axios.post(baseUrl , gestorSeleccionado)
+    .then((response) => {
+
+      setData(data.concat(response.data));
+      abrirCerrarModalInsertar();
+
+    }).catch((err) => {
+
+      console.log(err)
+    });
+  }
+
+
+  useEffect(()=>{
+
+    peticionGet();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br></br>
+      <button onClick={()=>abrirCerrarModalInsertar()}className="btn btn-success" >Insertar Nuevo Gestor</button>
+      <br></br>
+      <br></br>
+
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Lanzamiento</th>
+            <th>Desarrollador</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(gestor=>(
+            <tr>
+              <td>{gestor.id}</td>
+              <td>{gestor.nombre}</td>
+              <td>{gestor.lanzamiento}</td>
+              <td>{gestor.desarrollador}</td>
+              <td>
+                <button className="btn btn-primary">Editar.</button> {"  "}
+                <button className="btn btn-danger">Eliminar.</button>
+              </td>
+            </tr>
+
+
+          ))}
+        </tbody>
+      </table>
+      
+      <Modal isOpen={modalInsertar}>
+      <ModalHeader>Insertar Gestor de Base de Datos</ModalHeader>
+      <ModalBody>
+      <div className="form-group">
+          <label>Nombre: </label>
+          <br />
+          <input type="text" className="form-control" name="nombre"  onChange={handleChange}/>
+          <br />
+          <label>Lanzamiento: </label>
+          <br />
+          <input type="text" className="form-control" name="lanzamiento" onChange={handleChange}/>
+          <br />
+          <label>Desarrollador: </label>
+          <br />
+          <input type="text" className="form-control" name="desarrollador" onChange={handleChange}/>
+          <br />
+        </div>
+      </ModalBody>
+      <ModalFooter>
+        <button className="btn btn-primary" onClick={()=>peticionPost()}>Insertar</button>{"   "}
+        <button className="btn btn-danger" onClick={()=>abrirCerrarModalInsertar()}>Cancelar</button>
+      </ModalFooter>
+    </Modal>
+
     </div>
   );
 }
